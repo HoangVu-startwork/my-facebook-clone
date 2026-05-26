76146c6f9fd918d0d106e4f63c61b763afee3287
HoangVu-startwork <hoangvu.startwork@gmail.com>
2026-05-26 17:48:37 +0700

    cập nhat api backend

--- service/socket.js
+++ service/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:9090";
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "https://webpostsend.click/api";
 
 let socket = null;
 
