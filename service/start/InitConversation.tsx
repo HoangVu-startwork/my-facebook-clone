fdcd0b51cffd84c6f3cb04276e245c143c790671
HoangVu-startwork <hoangvu.startwork@gmail.com>
2026-08-15 22:49:43 +0700

    Chỉnh sửa chỉ đang nhập thành công mới get các dữ liệu

--- service/start/InitConversation.tsx
+++ service/start/InitConversation.tsx
@@ -2,15 +2,19 @@
 
 import { useEffect } from "react";
 import { useConversationStore } from "@/components/Message/conversationStoreNew";
+import { useAuthStore } from "@/service/service-once/AuthState";
 
 export default function InitConversation() {
+    const user = useAuthStore((state) => state.user);
     const fetchConversations = useConversationStore(
         (state) => state.fetchConversations
     );
 
     useEffect(() => {
+        if (!user) return;
+
         fetchConversations();
-    }, [fetchConversations]);
+    }, [user, fetchConversations]);
 
     return null;
 }
