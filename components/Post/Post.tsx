a147c6f9f1076c0dc14925092acc1b4a8b76ec58
HoangVu-startwork <hoangvu.startwork@gmail.com>
2026-08-25 23:52:48 +0700

    post

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -68,3 +68,94 @@ export interface ReactionResult {
     status: "added" | "removed" | "updated";
     reaction?: string;
 }
+
+
+export default function Post() {
+    const formatDateTime = (dateString: string) => {
+        const date = new Date(dateString);
+
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+
+        return `${hours}:${minutes}:${seconds} - ${day}/${month}/${year}`;
+    };
+
+    const user = useAuthStore((state) => state.user);
+    const socket = useSocketStore((s) => s.socket);
+    //const [user, setUser] = useState<any>(null); // user hiện tại
+    //const [socket, setSocket] = useState<any>(null);
+    const [messages, setMessages] = useState<string[]>([]);
+    const [msg, setMsg] = useState("");
+
+    const [mes, setMes] = useState("");
+
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const infiniteObserverRef = useRef<IntersectionObserver | null>(null);
+    const loadMoreRef = useRef<HTMLDivElement | null>(null);
+    const [seed] = useState(() => Date.now().toString());
+    const [dataPost, setDataPost] = useState<Postuser[]>([]);
+    const fetchPost = async (pageNumber = 1) => {
+        if (loading || !hasMore) return;
+
+        setLoading(true);
+        try {
+            const response = await Posts.getPost(pageNumber, 10, seed);
+            setLoading(false);
+            const newPosts = (response?.data?.data?.posts || []) as Postuser[];
+            const pagination = response?.data?.data?.pagination;
+            setDataPost(prev => [...prev, ...newPosts]);
+            setHasMore(pagination?.hasMore ?? false);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+    useEffect(() => {
+        fetchPost(1);
+    }, []);
+
+    useEffect(() => {
+        if (!hasMore || loading) return;
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    setPage(prev => prev + 1);
+                }
+            },
+            {
+                root: null,
+                rootMargin: "200px", // 👈 QUAN TRỌNG
+                threshold: 0
+            }
+        );
+
+        if (loadMoreRef.current) {
+            observer.observe(loadMoreRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, [hasMore, loading]);
+
+
+
+    useEffect(() => {
+        if (page > 1) {
+            fetchPost(page);
+        }
+    }, [page]);
+    return (
+        <>
+        </>
+    );
+}
